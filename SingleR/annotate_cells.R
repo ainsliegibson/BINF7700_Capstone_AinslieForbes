9c5d19e3d33a92a1b735e4961224361fd1539d10
Ainslie Forbes <ainslie.forbes.2000@gmail.com>
2026-04-14 10:10:44 -0400

    Add SingleR programs

---
+++ SingleR/annotate_cells.R
@@ -0,0 +1,32 @@
+BiocManager::install(version = "3.22", ask = FALSE)
+
+library(Seurat)
+library(SingleCellExperiment)
+library(SingleR)
+library(celldex)
+
+#pick one of the subset files
+#path_f<-"/data/subset_reclustered_relaxed.rds"
+path_f<-"/data/subset_reclustered_relaxed.rds"
+
+sub_obj<-readRDS(path_f)
+
+library(SingleCellExperiment)
+
+mat <- LayerData(sub_obj, assay = "SCT", layer = "data")
+
+sce <- SingleCellExperiment(mat)
+
+colData(sce)$orig.ident  <- sub_obj$orig.ident
+colData(sce)$cluster <- sub_obj$seurat_clusters
+
+ref.set <- celldex::HumanPrimaryCellAtlasData()
+
+pred.cnts <- SingleR::SingleR(test = sce, ref = ref.set, labels = ref.set$label.fine, assay.type.test=1)
+
+lbls.keep <- table(pred.cnts$labels)>10
+sub_obj$SingleR.labels <- ifelse(lbls.keep[pred.cnts$labels], pred.cnts$labels, 'Other')
+DimPlot(sub_obj, reduction='umap', group.by='SingleR.labels')
+
+table_of_values<-table(Cluster = sub_obj$seurat_clusters, Label = sub_obj$SingleR.labels)
+table_of_values
